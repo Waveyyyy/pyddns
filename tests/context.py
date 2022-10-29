4b2b401d3b0b937ce6247db2a47058e1c309f6f4
Waveyyyy <waves-sec@protonmail.com>
2022-10-29 14:00:29 +0100

    Give future test suite full import context

---
+++ tests/context.py
@@ -0,0 +1,10 @@
+# From the python filestructure guide
+# link: https://docs.python-guide.org/writing/structure/#test-suite
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirame(__file__), '..')))
+
+# import the library
+import ddns
