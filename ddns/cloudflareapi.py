e5f2b961d397c19b7644c4cd265a39b608161bd7
Waveyyyy <waves-sec@protonmail.com>
2022-10-29 17:14:00 +0100

    Moved everything into an Api class
    `Api()` class now has a dictionary called info, accessable with `self.info`
    or `Api().info`. Contains information that may be useful on future calls
    of functions, such as:
    - zoneid
    - userid
    - old_ip
    - new_ip
    - last_updated
    
    `verifyupdate()` is a private function now, and will most likely get used
    inside `updateip()`

--- ddns/cloudflareapi.py
+++ ddns/cloudflareapi.py
@@ -3,27 +3,33 @@ import requests
 import externip as pub
 
 
-def getzoneid():
-    """Get the zone identifier, needed to make most requests"""
-    pass
+class Api:
+    def __init__(self):
+        self.info = {
+            'zoneid': self.getzoneid(),
+            'userid': self.getuserid(),
+            'old_ip': self.getcurrentip(),
+            'new_ip': pub.externip(),
+            # 'last_updated': update_time,
 
+        }
 
-def getuserid():
-    """Get the user identifier, also needed to make most requests"""
-    pass
+    def getzoneid(self):
+        """Get the zone identifier, needed to make most requests"""
+        pass
 
+    def getuserid():
+        """Get the user identifier, also needed to make most requests"""
+        pass
 
-def getcurrentip():
-    """Get the current IP from the DNS A record"""
-    pass
+    def getcurrentip():
+        """Get the current IP from the DNS A record"""
+        pass
 
+    def updateip():
+        """Update the DNS A record with the IP returned by pub.getexternip()"""
+        pass
 
-def updateip():
-    """Update the DNS A record with the IP returned by pub.getexternip()"""
-    pass
-
-
-# might be an unnecessary function, evaluate usefulness
-def verifyupdate(getcurrentip):
-    """Verify if the DNS A record was successfully updated"""
-    pass
+    def __verifyupdate(getcurrentip):
+        """Verify if the DNS A record was successfully updated"""
+        pass
