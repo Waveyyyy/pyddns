45c3b498ed7ca237a76ba3bd7ad7bbf3c13bc0bb
Waveyyyy <Waves-sec@protonmail.com>
2023-03-25 17:39:02 +0000

    Removed getzoneid and getuserid, now supplied in dotenv file
    These are needed for requests and can be obtained from cloudflare
    dashboard but not the API, therefore they need to be handled as secrets.
    
    Secrets now required are:
    - AUTH_EMAIL
    - AUTHORIZATION
    - ZONEID
    - USERID

--- ddns/api.py
+++ ddns/api.py
@@ -7,18 +7,16 @@ from dotenv import dotenv_values
 class Api:
     def __init__(self):
         self.info = {
-            'zoneid': None,
-            'userid': None,
+            'zoneid': dotenv_values(".env")["ZONEID"],
+            'userid': dotenv_values(".env")["USERID"],
             'old_ip': None,
             'new_ip': pub.getexternip(),
             'domain': None,
             # 'last_updated': update_time,
-
         }
         # headers used for api requests
         self.Headers = {
             'X-Auth-Email': dotenv_values(".env")["AUTH_EMAIL"],
-            'X-Auth-Key': dotenv_values(".env")["API_TOKEN"],
             'Authorization': f'Bearer {dotenv_values(".env")["AUTHORIZATION"]}',
             'Content-Type': 'application/json',
         }
@@ -28,20 +26,6 @@ class Api:
         url_base = 'https://api.cloudflare.com/client/v4/'
         url = '/'.join([url_base, endpoint])
         return url
-
-    def getzoneid(self):
-        """Get the zone identifier, needed to make most requests"""
-        # curl -X GET "https://api.cloudflare.com/client/v4/zones" \
-        # -H "X-Auth-Email: user@example.com" \
-        # -H "X-Auth-Key: c2547eb745079dac9320b638f5e225cf483cc5cfdda41" \
-        # -H "Content-Type: application/json"
-        with requests.Session() as session:
-            pass
-        pass
-
-    def getuserid(self):
-        """Get the user identifier, also needed to make most requests"""
-        pass
 
     def getcurrentip(self):
         """Get the current IP from the DNS A record"""
@@ -55,6 +39,8 @@ class Api:
         """Verify if the DNS A record was successfully updated"""
         pass
 
+
 if __name__ == "__main__":
     a = Api()
     print(a.Headers)
+    print(a.info)
