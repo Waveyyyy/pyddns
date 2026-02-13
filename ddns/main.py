eb2f38de00df8a9d6a720035980cdf9173c82f02
waves <gitea@wavess.xyz>
2026-02-13 17:44:52 +0000

    Add error checks and return type to get_wan_ip
    Wrap functionality in try catch expression.
    Check explicitly for case where discover() returns 0.

--- ddns/main.py
+++ ddns/main.py
@@ -1,14 +1,21 @@
 import miniupnpc
+from typing import Optional
 
-def get_wan_ip() -> str:
+def get_wan_ip() -> Optional[str]:
     """
     Get WAN ip adress from router using uPnP
     """
-    upnp = miniupnpc.UPnP()
-    upnp.discoverdelay = 200
-    upnp.discover()
-    upnp.selectigd()
-    return upnp.externalipaddress()
+    try:
+        upnp = miniupnpc.UPnP()
+        upnp.discoverdelay = 200 # Discovery timeout (default: 2000)
+        if not upnp.discover(): # Number of devices (0 is a failure state)
+            return None
+
+        upnp.selectigd() # Select Internet Gateway Device (router)
+        return upnp.externalipaddress() # Return WAN IP as a string
+    except Exception:
+        return None
+
 
 if __name__ == "__main__":
     print(get_wan_ip())
