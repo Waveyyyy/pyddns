e791b5adbd27e329cbe0dab6bfc9387db7594d5d
waves <gitea@wavess.xyz>
2026-02-16 19:22:43 +0000

    Add logging in preparation for containerising

--- ddns/main.py
+++ ddns/main.py
@@ -5,6 +5,9 @@ import requests
 import json
 from os import getenv
 import time
+import logging
+import sys
+from pathlib import Path
 
 class ExternalIP():
 
@@ -17,6 +20,7 @@ class ExternalIP():
         """
         url = self.FALLBACK_PROVIDERS.get(provider)
         if not url:
+            log.error(f"Unknown provider '{provider}'")
             raise ValueError(f"Unknown provider '{provider}'. Options\n{list(self.FALLBACK_PROVIDERS.keys())}")
 
         try:
@@ -31,6 +35,7 @@ class ExternalIP():
             return wan_ip
 
         except requests.HTTPError:
+            log.warning(f"WAN IP http fallback failed for provider '{provider}'")
             return None
 
     def get_wan_ip_upnp(self, fallback: bool) -> Optional[str]:
@@ -44,14 +49,21 @@ class ExternalIP():
                 return None
 
             upnp.selectigd() # Select Internet Gateway Device (router)
-            return upnp.externalipaddress() # Return WAN IP as a string
+            wan_ip = upnp.externalipaddress()
+            log.info(f"WAN IP detected via UPnP: {wan_ip}")
+
+            return wan_ip # Return WAN IP as a string
 
         except Exception:
             if fallback:
+                log.warning("WAN IP detection via UPnP failed, trying http fallback")
                 for provider in self.FALLBACK_PROVIDERS.keys():
                     ip: str | None = self.get_wan_ip_http(provider)
                     if ip:
+                        log.info(f"WAN IP detected via http: {ip}")
                         return ip
+                    else:
+                        log.error("WAN IP detectian via http fallback failed")
 
 
 class Cloudflare():
@@ -70,23 +82,34 @@ class Cloudflare():
 
     def _get_record(self) -> tuple[str, str]:
         """Fetch record ID and current IP from Cloudflare. Called once on startup."""
-        resp = requests.get(
-            f"{self.cf_api}/zones/{self.zone_id}/dns_records",
-            headers = self.headers,
-            params = {"type": "A", "name": self.record_name}
-        )
-        resp.raise_for_status()
-        records = resp.json()["result"]
+        api_url: str = f"{self.cf_api}/zones/{self.zone_id}/dns_records"
 
-        if not records:
-            raise ValueError(f"No A record found for {self.record_name}")
+        try:
+            resp = requests.get(
+                url=api_url,
+                headers = self.headers,
+                params = {"type": "A", "name": self.record_name}
+            )
+            resp.raise_for_status()
+            records = resp.json()["result"]
 
-        return records[0]["id"], records[0]["content"]
+            if not records:
+                raise ValueError(f"No A record found for {self.record_name}")
+
+            log.info(f"record_id: {records[0]["id"]}")
+            log.info(f"record_content: {records[0]["content"]}")
+
+            return records[0]["id"], records[0]["content"]
+        except requests.HTTPError:
+            log.error(f"Failed to get Cloudflare A record at url: {api_url}")
+            return "", "" # rework
 
     def get_ip(self):
         """Get the current IP address from the cache, or fetch if not cached."""
         if not self.cached_ip:
             self.record_id, self.cached_ip = self._get_record()
+            log.info(f"Updated cached_ip: {self.cached_ip}")
+            log.info(f"Updated record_id: {self.record_id}")
 
         return self.cached_ip
 
@@ -94,33 +117,49 @@ class Cloudflare():
         """Update the A record and refresh the cache."""
         if self.record_id is None:
             self.record_id, self.cached_ip = self._get_record()
+            log.info(f"Updated cached_ip: {self.cached_ip}")
+            log.info(f"Updated record_id: {self.record_id}")
 
         # https://developers.cloudflare.com/api/resources/dns/subresources/records/methods/get/
-        resp = requests.patch(
-            f"{self.cf_api}/zones/{self.zone_id}/dns_records/{self.record_id}",
-            headers=self.headers,
-            json={
-                "type": "A",
-                "name": self.record_name,
-                "content": new_ip,
-                "ttl": 1,
-                "proxied": False
-            }
-        )
-        resp.raise_for_status()
+        api_url = f"{self.cf_api}/zones/{self.zone_id}/dns_records/{self.record_id}"
 
-        if resp.json()["success"]:
-            self.cached_ip = new_ip
-            return True
-        return False
+        try:
+            resp = requests.patch(
+                url=api_url,
+                headers=self.headers,
+                json={
+                    "type": "A",
+                    "name": self.record_name,
+                    "content": new_ip,
+                    "ttl": 1,
+                    "proxied": False
+                }
+            )
+            resp.raise_for_status()
+
+            if resp.json()["success"]:
+                self.cached_ip = new_ip
+                log.info(f"Cloudflare A record change successful")
+                return True
+            return False
+        except requests.HTTPError:
+            log.error(f"Failed to update Cloudflare A record at url: {api_url}")
+            return False
 
 
 if __name__ == "__main__":
-    cloudflare_data: dict[str, str | None] = dotenv_values(".cloudflare")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        stream=sys.stdout,
+    )
+    log = logging.getLogger("ddns")
+
     cf = Cloudflare(
-        api_token=cloudflare_data["CF_API_TOKEN"] if cloudflare_data["CF_API_TOKEN"] else str(getenv("CF_API_TOKEN")),
-        zone_id=cloudflare_data["CF_ZONE_ID"] if cloudflare_data["CF_ZONE_ID"] else str(getenv("CF_ZONE_ID")),
-        record_name=cloudflare_data["CF_RECORD_NAME"] if cloudflare_data["CF_RECORD_NAME"] else str(getenv("CF_RECORD_NAME"))
+        api_token=str(getenv("CF_API_TOKEN")),
+        zone_id=str(getenv("CF_ZONE_ID")),
+        record_name=str(getenv("CF_RECORD_NAME"))
     )
     timeout = int(getenv("TIMEOUT")) if getenv("TIMEOUT") else 600
     while True:
@@ -128,6 +167,7 @@ if __name__ == "__main__":
         dns_ip = cf.get_ip()
 
         if wan_ip != dns_ip:
+            log.info(f"IP changed: {dns_ip} -> {wan_ip}, updating Cloudflare A record")
             cf.update_ip(str(wan_ip))
-
         time.sleep(timeout)
+        Path("/tmp/ddns_healthy").touch()
